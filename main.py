3da5a05a7d7448b7be0636e3523ee650ea97974c
Do Xuan Long <longdo@dobtech.vn>
2023-10-31 18:44:16 +0700

    implement chat chain

--- main.py
+++ main.py
@@ -1,4 +1,24 @@
+from langchain.chat_models import ChatOpenAI
+from langchain.chains import LLMChain
+from langchain.prompts import ChatPromptTemplate, HumanMessagePromptTemplate
+from dotenv import load_dotenv
+
+load_dotenv()
+
+chat = ChatOpenAI()
+
+prompt = ChatPromptTemplate(
+    input_variables=["content"],
+    messages=[HumanMessagePromptTemplate.from_template("{content}")],
+)
+
+chain = LLMChain(
+    llm=chat,
+    prompt=prompt,
+)
+
 while True:
     content = input(">> ")
 
-    print(f"You entered: {content}")
+    result = chain({"content": content})
+    print(result["text"])
